4fcc52edf145cac7aeeec434aa512c4a022bafde
cyannyan47 <minhdinhdev47@gmail.com>
2020-10-21 00:36:02 -0700

    Finish terminal read/write

--- Assignment 2/test_read_write/test_read_write.c
+++ Assignment 2/test_read_write/test_read_write.c
@@ -4,13 +4,33 @@
 
 bool terminate_sequence(char *ch, bool *term_seq0, bool *term_seq1);
 
+// int main() {
+//     int sz;
+//     char ch;
+//     bool term_seq0, term_seq1;
+//     term_seq0 = true;
+//     term_seq1 = false;
+//     while (read(STDIN_FILENO, &ch, 1) > 0) {
+//         sz = write(STDOUT_FILENO, &ch, 1);
+//         if (terminate_sequence(&ch, &term_seq0, &term_seq1)) {
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
 int main() {
     int sz;
+    int rSz;
     char ch;
+    // term_seq0 true when ch stream has '\n' pattern
+    // term_seq1 true when ch stream has '\n!' pattern
     bool term_seq0, term_seq1;
+    // Assuming there will be a '\n' before the first char
     term_seq0 = true;
     term_seq1 = false;
-    while (read(STDIN_FILENO, &ch, 1) > 0) {
+    while (1) {
+        rSz = read(STDIN_FILENO, &ch, 1);
         sz = write(STDOUT_FILENO, &ch, 1);
         if (terminate_sequence(&ch, &term_seq0, &term_seq1)) {
             break;
@@ -18,6 +38,7 @@ int main() {
     }
     return 0;
 }
+
 
 bool terminate_sequence(char *ch, bool *term_seq0, bool *term_seq1) {
     if (*ch == '!') {
