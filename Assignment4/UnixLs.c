0d6b000f4bb6610605d383d492661396bbd90c0f
cyannyan47 <minhdinhdev47@gmail.com>
2020-11-28 14:43:33 -0800

    Using readdir

--- Assignment4/UnixLs.c
+++ Assignment4/UnixLs.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <libgen.h>
 
 int main(int argc, char* argv[]) {
     printf("Using UnixLs\n");
+    DIR *dir;
+    struct dirent *dp;
+
+    if ((dir = opendir (".")) == NULL) {
+        perror ("Cannot open .");
+        exit (1);
+    }
+
+    while ((dp = readdir (dir)) != NULL) {
+        printf("%s\n", dp->d_name);
+    }
+
+    closedir(dir);
     return 0;
 }
